3ff9ac9bee7f1d87dae8d39e2f5750e83b52bdae
IvoAtanasov <ivoatanasov85@gmail.com>
2015-01-27 11:21:29 +0200

    Add Files

---
+++ DairyDataLayer/DairyDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace DairyDataLayer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Data.Entity;
+    using DairyModelLayer;
+    class DairyDbContext:DbContext
+    {
+        public DbSet<Story> Story { get; set; }
+        public DbSet<MySelf> MySelf { get; set; }
+    }
+}
