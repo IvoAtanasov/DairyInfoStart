bd10a29eafeb5c96de1cb35b8c5b9ec550aaffbf
IvoAtanasov <ivoatanasov85@gmail.com>
2015-01-27 17:13:49 +0200

    Add bundles and optimization

--- DairyWeb/Global.asax.cs
+++ DairyWeb/Global.asax.cs
@@ -1,9 +1,11 @@
-﻿using DairyWeb.Infrastructure;
+﻿using DairyWeb.App_Start;
+using DairyWeb.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace DairyWeb
@@ -15,6 +17,7 @@ namespace DairyWeb
             AreaRegistration.RegisterAllAreas();
             AutoMapperConfig.Execute();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
